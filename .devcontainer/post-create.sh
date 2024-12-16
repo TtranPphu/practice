4d23d7ff570361433342d68fa655986326e30601
Phu Tran <ttranpphu0212@outlook.com>
2024-12-16 10:37:43 +0000

    rust: init

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,3 +1,4 @@
 #! /usr/bin/sh
 
 pip install --no-warn-script-location -r python/requirement
+# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
--- python/test_cases/__init__.py
+++ python/test_cases/__init__.py
@@ -1,9 +1,9 @@
-# from .leetcode_find_median_sorted_arrays_test import LeetcodeFindMedianSortedArraysTest
+from .leetcode_find_median_sorted_arrays_test import LeetcodeFindMedianSortedArraysTest
 
-# from .leetcode_length_of_longest_substring_test import (
-#     LeetcodeLengthOfLongestSubstringTest,
-# )
+from .leetcode_length_of_longest_substring_test import (
+    LeetcodeLengthOfLongestSubstringTest,
+)
 
-# from .leetcode_bulb_switch_test import LeetcodeBulbSwitchTest
-# from .leetcode_height_checker_test import LeetcodeHeightCheckerTest
+from .leetcode_bulb_switch_test import LeetcodeBulbSwitchTest
+from .leetcode_height_checker_test import LeetcodeHeightCheckerTest
 from .leetcode_is_long_pressed_name_test import LeetcodeIsLongPressedNameTest
