78d2d6da5a07a46eea4ad6e6cc88ef83fceb1beb
Phu Tran <ttranpphu0212@outlook.com>
2025-01-08 05:31:56 +0000

    rust: some leetcode

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,12 @@
+pub mod my_pow;
 fn main() {
-    println!("Hello, world!");
+    test_my_pow();
+}
+
+fn test_my_pow() {
+    use crate::my_pow::my_pow::Solution;
+    let x = 2.0;
+    let n = 10;
+    let result = Solution::my_pow(x, n);
+    println!("my_pow({}, {}): {}", x, n, result);
 }
