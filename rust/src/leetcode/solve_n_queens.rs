2b4fcf93473f562298b98e3557af3b1b167b7d89
Phu Tran <ttranpphu0212@outlook.com>
2025-01-15 16:40:42 +0000

    feat: update project configuration; add Rust build and test tasks, modify Python task labels, and enhance .gitignore

--- rust/src/leetcode/solve_n_queens.rs
+++ rust/src/leetcode/solve_n_queens.rs
@@ -39,3 +39,26 @@ pub mod solve_n_queens {
         }
     }
 }
+
+#[cfg(test)]
+mod leetcode_test {
+    use crate::leetcode::solve_n_queens::solve_n_queens::Solution;
+
+    #[test]
+    fn test_solve_n_queens() {
+        let cases = vec![
+            (
+                4,
+                vec![
+                    vec![".Q..", "...Q", "Q...", "..Q."],
+                    vec!["..Q.", "Q...", "...Q", ".Q.."],
+                ],
+            ),
+            (2, vec![]),
+        ];
+        for (n, expected) in cases {
+            let result = Solution::solve_n_queens(n);
+            assert_eq!(result, expected);
+        }
+    }
+}
