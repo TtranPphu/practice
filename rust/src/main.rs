79545aea60aa5aacdc33e2ed53d763ab197880a1
Phu Tran <ttranpphu0212@outlook.com>
2025-01-30 11:34:33 +0000

    feat: implement comprehension macro; add module and example usage in main

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,13 @@
 mod leetcode;
+use macro_utils::comprehension;
 
-fn main() {}
+fn main() {
+    let deck = comprehension! {
+        format!("{}{}", rank, suit)
+        for rank in ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+        for suit in ["♠", "♣", "♥", "♦"]
+    };
+    for card in deck {
+        println!("{}", card);
+    }
+}
