20f3f4e5e42a7d36c91b3e60d24ad7074fb93dc8
TtranPphu <ttranpphu0212@gmail.com>
2025-03-20 04:04:07 +0000

    feat: update Dockerfile and VSCode settings; enhance environment setup with neo-vim and updated dependencies

--- .devcontainer/codespace/post-create-codespace.sh
+++ .devcontainer/codespace/post-create-codespace.sh
@@ -1,5 +1,4 @@
 #! /usr/bin/sh
 
-cd python; uv sync; cd ..
+cd python; uv python install "$(< .python-version)" ; uv sync; cd ..
 cp .devcontainer/codespace/.p10k.zsh ~/.p10k.zsh
-cp .devcontainer/.zshrc ~/.zshrc
--- .devcontainer/standalone/post-create-standalone.sh
+++ .devcontainer/standalone/post-create-standalone.sh
@@ -2,4 +2,3 @@
 
 cd python; uv sync; cd ..
 cp .devcontainer/standalone/.p10k.zsh ~/.p10k.zsh
-cp .devcontainer/.zshrc ~/.zshrc
