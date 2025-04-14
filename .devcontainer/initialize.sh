83e4e54dd69e0753d2d21c245213475d6d78eb9c
TtranPphu <ttranpphu0212@gmail.com>
2025-04-14 01:58:37 +0000

    feat: Refactor devcontainer setup; update configurations, add Dockerfile, and initialize scripts

--- .devcontainer/initialize.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/bash
-
-cp .devcontainer/docker-compose.yml.sample .devcontainer/docker-compose.yml
-sed -i "s/<host-user-name>/$USER/g" .devcontainer/docker-compose.yml
-
-# dummy nvim config
-mkdir ~/.config/nvim 2>/dev/null | true
---
+++ start-nvim-container.sh
@@ -0,0 +1,2 @@
+sh initialize.sh && docker compose up -d devcontainer --build && docker exec -it --workdir /workspaces/practice practice-devcontainer-1 nvim
+
